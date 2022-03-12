16fb77cabddd21c792f330c0416414a82a0caa0c
Nithiya Soundari Kirubakaran <nithiyak@Nithiyas-MacBook-Air.local>
2022-03-12 22:04:52 +0800

    Controller Update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,34 +13,6 @@ class Controller extends BaseController
     const PAID = "PAID";
     const PENDING = "PENDING";
     const APPROVED = "APPROVED";
-    
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    // Unauthorized Error Response
-    public function returnUnauthorizedResponse(){
-        return response() -> json([
-            'message' => 'You are unauthorized to perform this action.',
-            'error' => true,
-            'code' => 401
-        ], 401);
-    }
-
-    // Error response with 
-    public function returnErrorRespose($msg){
-        return response() -> json([
-            'message' => $msg,
-            'error' => true,
-            'code' => 400
-        ], 400);
-    }
-
-    public function returnSuccessResponse($msg, $data){
-        return response() -> json([
-            'message' => $msg,
-            'result' => $data,
-            'code' => 200
-        ], 200);
-    }
 
     public function returnResponse($msg, $code, $err){
         $output = array(
@@ -51,27 +23,4 @@ class Controller extends BaseController
 
         return response() -> json([$output], $code);
     }
-
-
-    public function returnPositiveRespose($msg, $code){
-        $successOutput = array(
-            'error' => false,
-            'code' => $code
-        ); 
-        $successOutput = array_merge($msg, $successOutput);
-
-        return response() -> json([$errorOutput], $code);
-    }   
-
-    public function returnErrRespose($msg, $code){
-        $errorOutput = array(
-            'error' => true,
-            'code' => $code
-        ); 
-        $errorOutput = array_merge($msg, $errorOutput);
-
-        return response() -> json([$errorOutput], $code);
-    }   
-
-
 }
