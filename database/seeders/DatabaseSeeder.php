22194c335341d39c35a7f0b9efefc0e83af238c5
Nithiya Soundari Kirubakaran <nithiyak@Nithiyas-MacBook-Air.local>
2022-03-13 16:47:23 +0800

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        for($i=0; $i<5; $i++){
+            DB::table('users')->insert([
+                'user_id'=>\Ramsey\Uuid\Uuid::uuid4()->toString(),
+                'name'=>Str::random(15),
+                'user_type'=>"Client",
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
+        for($i=0; $i<2; $i++){
+            DB::table('users')->insert([
+                'user_id'=>\Ramsey\Uuid\Uuid::uuid4()->toString(),
+                'name'=>Str::random(15),
+                'user_type'=>"Admin",
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
     }
 }
